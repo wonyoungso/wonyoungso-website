db9c37f559b97fa34cb0262e1d4a0443c57eadf0
Wonyoung So <wso@mit.edu>
2022-01-31 14:50:39 -0500

    update

--- src/app/pages/Profile.js
+++ src/app/pages/Profile.js
@@ -39,19 +39,12 @@ class Firstpage extends Component {
             <div className="profile-desc-cv__desc">
               <p className="profile-paragraph" style={fgStyle}>
                 Now I am a Ph.D. student in Urban Information Systems at the Department of
-                Urban Studies and Planning (DUSP) at the Massachusetts Institute of Technology (MIT), where I was a presidential fellow. I am also working as a research assistant and technical lead at the Data + Feminism Lab at MIT. In 2011,
-                I co-founded the crowdfunding platform Tumblbug and demonstrated an alternative about
-                the sustainability of the DIY movement. I was a research fellow at at the Senseable City Lab
-                at MIT, and I co-curated Seoul Libre Maps in Seoul Biennale of Architecture and Urbanism
-                for realizing the vision of a citizen-organized data community. I hold a Master in City Planning at MIT DUSP, a BFA in Visual Communication Design from Kookmin University, and
-                I studied at the School for Poetic Computation in New York City. My work has been recognized by Information is Beautiful Awards, Adobe, MIT Museum, the Seoul Biennale of Archiecture and
-                Urbanism, IEEE, Fast Co. Design, The Atlantic, CNN, The Guardian, Seoul Museum of Art
-                and Wired, among others.
+                Urban Studies and Planning (DUSP) at the Massachusetts Institute of Technology (MIT), where I was a presidential fellow. I am also working as a research assistant and technical lead at the Data + Feminism Lab at MIT. In 2011, I co-founded the crowdfunding platform Tumblbug and demonstrated an alternative about the sustainability of the DIY movement. I was a research fellow at at the Senseable City Lab at MIT, and I co-curated Seoul Libre Maps in Seoul Biennale of Architecture and Urbanism for realizing the vision of a citizen-organized data community. I hold a Master in City Planning at MIT DUSP, a BFA in Visual Communication Design from Kookmin University, and I studied at the School for Poetic Computation in New York City. My work has been recognized by Information is Beautiful Awards, Adobe, MIT Museum, the Seoul Biennale of Archiecture and Urbanism, IEEE, Fast Co. Design, The Atlantic, CNN, The Guardian, Seoul Museum of Art and Wired, among others.
               </p>
             </div>
             <div className="profile-desc-cv__cv">
               <p className="profile-paragraph">
-                <a href="/cv_20210829.pdf" target="_blank" style={fgStyle}>
+                <a href="/cv_20220131.pdf" target="_blank" style={fgStyle}>
                   CV
                 </a>
               </p>
